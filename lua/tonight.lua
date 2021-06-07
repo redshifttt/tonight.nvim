8adcf0b44a16b9f09d7b2033b9321f4861bcc01b
numpadd <num@privatevoid.net>
2021-06-08 00:44:10 +0100

    testing

---
+++ lua/tonight.lua
@@ -0,0 +1,37 @@
+local colors = {
+    bg_colour      = #0c0e11;
+    fg_colour      = #d3e3f5;
+    gray           = #2c3641;
+    red            = #ec3e3e;
+    red_darker     = #d52828;
+    green          = #61c15c;
+    green_darker   = #3fb439;
+    yellow         = #f0ea6a;
+    yellow_darker  = #ddbc3d;
+    blue           = #759ed4;
+    blue_darker    = #538bd4;
+    magenta        = #a588c6;
+    magenta_darker = #9163c6;
+    teal           = #50b2c1;
+    teal_darker    = #259caf;
+    white          = #d3e3f5;
+}
+
+vim.g.terminal_color_background = bg_colour
+vim.g.terminal_color_foreground = fg_colour
+vim.g.terminal_color_0          = bg_colour
+vim.g.terminal_color_1          = red
+vim.g.terminal_color_2          = green
+vim.g.terminal_color_3          = yellow
+vim.g.terminal_color_4          = blue
+vim.g.terminal_color_5          = magenta
+vim.g.terminal_color_6          = teal
+vim.g.terminal_color_7          = white
+vim.g.terminal_color_8          = gray
+vim.g.terminal_color_9          = red_darker
+vim.g.terminal_color_10         = green_darker
+vim.g.terminal_color_11         = yellow_darker
+vim.g.terminal_color_12         = blue_darker
+vim.g.terminal_color_13         = magenta_darker
+vim.g.terminal_color_14         = teal_darker
+vim.g.terminal_color_15         = white
