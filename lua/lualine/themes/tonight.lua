05087ed73aec1f4bc821d698e72dac2e2a1222c2
seanjbl <seanjblair1@gmail.com>
2022-01-21 10:43:30 +0000

    lualine: changed theme slightly

--- lua/lualine/themes/tonight.lua
+++ lua/lualine/themes/tonight.lua
@@ -1,34 +1,34 @@
 require('colours')
 
 return {
-  normal = {
-    a = { bg = c.magenta, fg = c.background, gui = 'bold' },
-    b = { bg = c.gray2, fg = c.magenta2 },
-    c = { bg = c.gray2, fg = c.gray }
-  },
-  insert = {
-    a = { bg = c.green, fg = c.background, gui = 'bold' },
-    b = { bg = c.gray2, fg = c.green },
-    c = { bg = c.gray2, fg = c.gray }
-  },
-  visual = {
-    a = { bg = c.yellow, fg = c.background, gui = 'bold' },
-    b = { bg = c.gray2, fg = c.yellow },
-    c = { bg = c.gray2, fg = c.gray }
-  },
-  replace = {
-    a = { bg = c.red2, fg = c.background, gui = 'bold' },
-    b = { bg = c.gray2, fg = c.red2 },
-    c = { bg = c.gray2, fg = c.gray }
-  },
-  command = {
-    a = { bg = c.red, fg = c.background, gui = 'bold' },
-    b = { bg = c.gray2, fg = c.red },
-    c = { bg = c.gray2, fg = c.gray }
-  },
-  inactive = {
-    a = { bg = c.gray2, fg = c.gray, gui = 'bold' },
-    b = { bg = c.gray2, fg = c.gray },
-    c = { bg = c.gray2, fg = c.gray }
-  }
+    normal = {
+        a = { bg = c.magenta, fg = c.background, gui = 'bold' },
+        b = { bg = c.background, fg = c.magenta },
+        c = { bg = c.background, fg = c.gray }
+    },
+    insert = {
+        a = { bg = c.green, fg = c.background, gui = 'bold' },
+        b = { bg = c.background, fg = c.green },
+        c = { bg = c.background, fg = c.gray }
+    },
+    visual = {
+        a = { bg = c.yellow, fg = c.background, gui = 'bold' },
+        b = { bg = c.background, fg = c.yellow },
+        c = { bg = c.background, fg = c.gray }
+    },
+    replace = {
+        a = { bg = c.red, fg = c.background, gui = 'bold' },
+        b = { bg = c.background, fg = c.red },
+        c = { bg = c.background, fg = c.gray }
+    },
+    command = {
+        a = { bg = c.red, fg = c.background, gui = 'bold' },
+        b = { bg = c.background, fg = c.red },
+        c = { bg = c.background, fg = c.gray }
+    },
+    inactive = {
+        a = { bg = c.background, fg = c.gray, gui = 'bold' },
+        b = { bg = c.background, fg = c.gray },
+        c = { bg = c.background, fg = c.gray }
+    }
 }
