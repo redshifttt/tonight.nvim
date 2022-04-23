5363db7015c454cbb7f358170d772cad79078be3
redshifttt <seanjblair1@gmail.com>
2022-04-23 01:03:26 +0100

    lualine: finish theme change

--- lua/lualine/themes/tonight.lua
+++ lua/lualine/themes/tonight.lua
@@ -2,33 +2,33 @@ local c = require('colours')
 
 return {
     normal = {
-        a = { bg = c.magenta, fg = c.background, gui = 'bold' },
-        b = { bg = c.background, fg = c.magenta },
-        c = { bg = c.background, fg = c.gray }
+        a = { bg = c.blue, fg = c.background, gui = 'bold' },
+        b = { bg = c.background_lighter, fg = c.blue },
+        c = { bg = c.background_lighter, fg = c.blue }
     },
     insert = {
         a = { bg = c.green, fg = c.background, gui = 'bold' },
-        b = { bg = c.background, fg = c.green },
-        c = { bg = c.background, fg = c.gray }
+        b = { bg = c.background_lighter, fg = c.green },
+        c = { bg = c.background_lighter, fg = c.green }
     },
     visual = {
         a = { bg = c.yellow, fg = c.background, gui = 'bold' },
-        b = { bg = c.background, fg = c.yellow },
-        c = { bg = c.background, fg = c.gray }
+        b = { bg = c.background_lighter, fg = c.yellow },
+        c = { bg = c.background_lighter, fg = c.yellow }
     },
     replace = {
         a = { bg = c.red, fg = c.background, gui = 'bold' },
-        b = { bg = c.background, fg = c.red },
-        c = { bg = c.background, fg = c.gray }
+        b = { bg = c.background_lighter, fg = c.red },
+        c = { bg = c.background_lighter, fg = c.red }
     },
     command = {
-        a = { bg = c.red, fg = c.background, gui = 'bold' },
-        b = { bg = c.background, fg = c.red },
-        c = { bg = c.background, fg = c.gray }
+        a = { bg = c.magenta, fg = c.background, gui = 'bold' },
+        b = { bg = c.background_lighter, fg = c.magenta },
+        c = { bg = c.background_lighter, fg = c.magenta }
     },
     inactive = {
-        a = { bg = c.background, fg = c.gray, gui = 'bold' },
-        b = { bg = c.background, fg = c.gray },
-        c = { bg = c.background, fg = c.gray }
+        a = { bg = c.background_lighter, fg = c.background, gui = 'bold' },
+        b = { bg = c.background_lighter, fg = c.background },
+        c = { bg = c.background_lighter, fg = c.background }
     }
 }
