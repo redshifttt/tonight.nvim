ae607d844c9e9410ceea690178b105c62d4e1dcb
redshifttt <num@privatevoid.net>
2022-07-21 22:54:45 +0100

    lualine: format lualine theme and tweak colours

--- lua/lualine/themes/tonight.lua
+++ lua/lualine/themes/tonight.lua
@@ -2,33 +2,93 @@ local c = require('colours')
 
 return {
     normal = {
-        a = { bg = c.blue, fg = c.background, gui = 'bold' },
-        b = { bg = c.background_lighter, fg = c.blue },
-        c = { bg = c.background_lighter, fg = c.blue }
+        a = {
+            bg = c.magenta,
+            fg = c.background,
+            gui = 'bold'
+        },
+        b = {
+            bg = c.background_lighter,
+            fg = c.magenta
+        },
+        c = {
+            bg = c.background_lighter,
+            fg = c.magenta
+        }
     },
     insert = {
-        a = { bg = c.green, fg = c.background, gui = 'bold' },
-        b = { bg = c.background_lighter, fg = c.green },
-        c = { bg = c.background_lighter, fg = c.green }
+        a = {
+            bg = c.green,
+            fg = c.background,
+            gui = 'bold'
+        },
+        b = {
+            bg = c.background_lighter,
+            fg = c.green
+        },
+        c = {
+            bg = c.background_lighter,
+            fg = c.green
+        }
     },
     visual = {
-        a = { bg = c.yellow, fg = c.background, gui = 'bold' },
-        b = { bg = c.background_lighter, fg = c.yellow },
-        c = { bg = c.background_lighter, fg = c.yellow }
+        a = {
+            bg = c.yellow,
+            fg = c.background,
+            gui = 'bold'
+        },
+        b = {
+            bg = c.background_lighter,
+            fg = c.yellow
+        },
+        c = {
+            bg = c.background_lighter,
+            fg = c.yellow
+        }
     },
     replace = {
-        a = { bg = c.red, fg = c.background, gui = 'bold' },
-        b = { bg = c.background_lighter, fg = c.red },
-        c = { bg = c.background_lighter, fg = c.red }
+        a = {
+            bg = c.red,
+            fg = c.background,
+            gui = 'bold'
+        },
+        b = {
+            bg = c.background_lighter,
+            fg = c.red
+        },
+        c = {
+            bg = c.background_lighter,
+            fg = c.red
+        }
     },
     command = {
-        a = { bg = c.magenta, fg = c.background, gui = 'bold' },
-        b = { bg = c.background_lighter, fg = c.magenta },
-        c = { bg = c.background_lighter, fg = c.magenta }
+        a = {
+            bg = c.blue,
+            fg = c.background,
+            gui = 'bold'
+        },
+        b = {
+            bg = c.background_lighter,
+            fg = c.blue
+        },
+        c = {
+            bg = c.background_lighter,
+            fg = c.blue
+        }
     },
     inactive = {
-        a = { bg = c.background_lighter, fg = c.background, gui = 'bold' },
-        b = { bg = c.background_lighter, fg = c.background },
-        c = { bg = c.background_lighter, fg = c.background }
+        a = {
+            bg = c.background_lighter,
+            fg = c.background,
+            gui = 'bold'
+        },
+        b = {
+            bg = c.background_lighter,
+            fg = c.background
+        },
+        c = {
+            bg = c.background_lighter,
+            fg = c.background
+        }
     }
 }
