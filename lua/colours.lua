17f8e7683c19f2152473242b674b5f384241ced2
redshifttt <num@privatevoid.net>
2022-08-17 15:15:07 +0100

    try and unify colours a bit more

--- lua/colours.lua
+++ lua/colours.lua
@@ -1,22 +1,23 @@
 local colours = {}
 
 -- base colours
-colours.background = "#0B0B12"
-colours.foreground = "#BEBAC6"
-colours.gray = "#3d4b5b"
+colours.background = "#0b0d12"
+colours.foreground = "#bebac6"
+colours.gray = "#585c67"
 colours.red = "#EC5959"
 colours.green = "#56d057"
 colours.yellow = "#D2CE6B"
-colours.blue = "#759ed4"
+colours.orange = "#CC8400"
+colours.blue = "#6494d4"
 colours.magenta = "#9D82E8"
 colours.teal = "#50b2c1"
 colours.white = "#BEBAC6"
 
 -- Special
-colours.background_lighter = "#151522"
-colours.highlight = colours.background_lighter
-colours.illuminate = "#181A1C"
-colours.indent_line = "#1B2129"
+colours.background_1 = "#1B2129"
+colours.background_2 = "#151522"
+colours.highlight = colours.background_1
+colours.illuminate = colours.background_2
 
 -- LSP
 colours.lsp_error = colours.red
