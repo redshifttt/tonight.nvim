344fa23b33ace8f12f165a9806b7fe1acdb7798b
redshifttt <num@privatevoid.net>
2022-08-19 20:21:18 +0100

    create function to link hl groups

--- lua/tonight/util.lua
+++ lua/tonight/util.lua
@@ -1,17 +1,20 @@
 M = {}
 
-function M.highlight(group, styles)
-    local gui = styles.gui
-        and 'gui=' .. styles.gui
-        or 'gui=NONE'
-    local fg = styles.fg
-        and 'guifg=' .. styles.fg
-        or 'guifg=NONE'
-    local bg = styles.bg
-        and 'guibg=' .. styles.bg
-        or 'guibg=NONE'
+local function linker(from_group, to_group)
+	vim.cmd("highlight! " .. "link " .. from_group .. " " .. to_group)
+end
 
-    vim.cmd('highlight ' .. group .. ' ' .. gui .. ' ' .. ' ' .. fg .. ' ' .. bg)
+function M.highlight(group, styles)
+    local gui = styles.gui and 'gui=' .. styles.gui or 'gui=NONE'
+    local fg = styles.fg and 'guifg=' .. styles.fg or 'guifg=NONE'
+    local bg = styles.bg and 'guibg=' .. styles.bg or 'guibg=NONE'
+	local link = styles.link or nil
+
+	if link then
+		linker(group, link)
+	else
+		vim.cmd('highlight ' .. group .. ' ' .. gui .. ' ' .. ' ' .. fg .. ' ' .. bg)
+	end
 end
 
 return M
