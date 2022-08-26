fcc0265b9b37e760e4f481a5de168f12818affc2
redshifttt <num@privatevoid.net>
2022-08-26 23:36:04 +0100

    formatting

--- lua/lualine/themes/tonight.lua
+++ lua/lualine/themes/tonight.lua
@@ -2,93 +2,39 @@ local c = require('tonight.colours')
 
 return {
     normal = {
-        a = {
-            bg = c.magenta,
-            fg = c.background,
-            gui = 'bold'
-        },
-        b = {
-            bg = c.background_02,
-            fg = c.magenta
-        },
-        c = {
-            bg = c.background_02,
-            fg = c.magenta
-        }
+        a = { bg = c.magenta, fg = c.background, gui = 'bold' },
+        b = { bg = c.background_02, fg = c.magenta },
+        c = { bg = c.background_02, fg = c.magenta },
+        z = { bg = c.background_02, fg = c.magenta }
     },
     insert = {
-        a = {
-            bg = c.green,
-            fg = c.background,
-            gui = 'bold'
-        },
-        b = {
-            bg = c.background_02,
-            fg = c.green
-        },
-        c = {
-            bg = c.background_02,
-            fg = c.green
-        }
+        a = { bg = c.green, fg = c.background, gui = 'bold' },
+        b = { bg = c.background_02, fg = c.green },
+        c = { bg = c.background_02, fg = c.green },
+        z = { bg = c.background_02, fg = c.green }
     },
     visual = {
-        a = {
-            bg = c.yellow,
-            fg = c.background,
-            gui = 'bold'
-        },
-        b = {
-            bg = c.background_02,
-            fg = c.yellow
-        },
-        c = {
-            bg = c.background_02,
-            fg = c.yellow
-        }
+        a = { bg = c.yellow, fg = c.background, gui = 'bold' },
+        b = { bg = c.background_02, fg = c.yellow },
+        c = { bg = c.background_02, fg = c.yellow },
+        z = { bg = c.background_02, fg = c.yellow }
     },
     replace = {
-        a = {
-            bg = c.red,
-            fg = c.background,
-            gui = 'bold'
-        },
-        b = {
-            bg = c.background_02,
-            fg = c.red
-        },
-        c = {
-            bg = c.background_02,
-            fg = c.red
-        }
+        a = { bg = c.red, fg = c.background, gui = 'bold' },
+        b = { bg = c.background_02, fg = c.red },
+        c = { bg = c.background_02, fg = c.red },
+        z = { bg = c.background_02, fg = c.red }
     },
     command = {
-        a = {
-            bg = c.blue,
-            fg = c.background,
-            gui = 'bold'
-        },
-        b = {
-            bg = c.background_02,
-            fg = c.blue
-        },
-        c = {
-            bg = c.background_02,
-            fg = c.blue
-        }
+        a = { bg = c.blue, fg = c.background, gui = 'bold' },
+        b = { bg = c.background_02, fg = c.blue },
+        c = { bg = c.background_02, fg = c.blue },
+        z = { bg = c.background_02, fg = c.blue }
     },
     inactive = {
-        a = {
-            bg = c.background_02,
-            fg = c.background,
-            gui = 'bold'
-        },
-        b = {
-            bg = c.background_02,
-            fg = c.background
-        },
-        c = {
-            bg = c.background_02,
-            fg = c.background
-        }
+        a = { bg = c.background_02, fg = c.background, gui = 'bold' },
+        b = { bg = c.background_02, fg = c.background },
+        c = { bg = c.background_02, fg = c.background },
+        z = { bg = c.background_02, fg = c.background }
     }
 }
